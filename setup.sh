8389b24d5e4db59bdda6b34b89fb5f9ec2752e6b
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2021-10-21 11:03:03 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,15 +9,13 @@ echo "---IMPORTANT--- If you do not wish to block Google (because blocking Googl
 echo "---TIP--- You should replace your DNS nameserver from 8.8.8.8 & 8.8.4.4 to 1.1.1.1 & 1.0.0.1 to avoid Google tracking your DNS queries.";
 echo "---TRICK--- If you have a problem connecting a certain website, go to https://hackertarget.com/find-dns-host-records/ find all corresponding CIDR, delete them and re-run setup.sh.";
 read -r -p "Do you want to continue? [Y/n] " input 
-case $input in ([yY][eE][sS]|[yY])
 sudo apt update && sudo apt install iptables -y;
 echo Flushing INPUT and OUTPUT tables.;
 sudo iptables -F INPUT;
 sudo iptables -F OUTPUT;
 echo Now adding CIDR IP blocks to iptables.;
-for i in `cat CIDR.txt`; do sudo iptables -I OUTPUT -s $i -j DROP | sudo iptables -I INPUT -s $i -j DROP | echo $i was successfully added to iptables configuration. ; done;
+for i in `cat CIDR.txt`; do iptables -I OUTPUT -s $i -j DROP -w && iptables -I INPUT -s $i -j DROP -w | echo $i was successfully added to iptables configuration. ; done;
 echo Saving iptables configuration permanently.;
 sudo iptables-save >config.log;
 echo Configuration log file generated to ./config.log;
 echo ALL DONE! Enjoy blocking most internet nuisances.;
-esac
